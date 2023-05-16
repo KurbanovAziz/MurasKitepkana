2f646c41cfaf49e1c94d4deb309ce4e3118bb926
Aziga04 <azizmusaev33@gmail.com>
2023-05-16 22:26:04 +0600

    registration and authorization page

--- app/src/main/java/com/example/muraskitepkana/MainActivity.kt
+++ app/src/main/java/com/example/muraskitepkana/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.muraskitepkana
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -8,10 +10,15 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.muraskitepkana.databinding.ActivityMainBinding
+import com.example.muraskitepkana.ui.register.PasswordRecoveryFragment
+import com.example.muraskitepkana.ui.register.RegisterFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var authFragment: AuthFragment
+    private lateinit var regFragment: RegisterFragment
+    private lateinit var loginFragment: PasswordRecoveryFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
